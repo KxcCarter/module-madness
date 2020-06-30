6c32dd4b0886fcff594adfef7a004b5fa1419a68
Kenneth Carter <kxccarter@gmail.com>
2020-06-30 18:40:50 -0500

    completes stretch goal 1, returning max on the 7th call and min on the 10th.

--- server/random-number.js
+++ server/random-number.js
@@ -1,10 +1,14 @@
 let timesCalled = 0;
 
 let randomNumber = (min, max) => {
+    timesCalled++;
     if (timesCalled == 7) {
         console.log(`you called it ${timesCalled} times!`);
+        return max;
+    } else if (timesCalled == 10) {
+        return min;
     }
-    timesCalled++;
+
     console.log(timesCalled);
 
     return Math.floor(Math.random() * (1 + max - min) + min);
